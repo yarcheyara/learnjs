5271ff377d376834370682ea3d8c22640c07f7d5
Yarche Yara <yarcheyara@gmail.com>
2025-11-22 18:02:27 +0200

    go

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 console.log('Hello JS world!');
+
+for(let i = 0; i < 100; i++) {
+    if(i % 3 == 0) {
+        console.log('Num: ' + i + ' - Fizz');
+    } else if ( i % 5 == 0) {
+        console.log('Num: ' + i + ' - Buzz');
+    } else if ( i % 3 == 0 && i % 5 == 0) {
+        console.log('Num: ' + i + ' - FizzBuzz');
+    }
+}
